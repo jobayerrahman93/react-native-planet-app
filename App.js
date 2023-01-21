480e4f1003069544968c17b8e1348f2ec3b1009a
jobayercse71 <rahmanjobayer71@gmail.com>
2023-01-21 14:09:07 +0600

    preset add and typography added

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Text from './src/components/text/text';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+  //   'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  //   'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+  // });
+
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+          'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+          'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+        });
+      
+        await new Promise(resolve => setTimeout(resolve,5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}  onLayout={onLayoutRootView} >
+      <Text preset='h1' >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +62,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
